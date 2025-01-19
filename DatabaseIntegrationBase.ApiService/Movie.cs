4e0719fba3247368e272fb978653f45f8bf74d1c
Edgard Domingues <degased@hotmail.com>
2025-01-19 15:53:03 -0300

    add movie class

---
+++ DatabaseIntegrationBase.ApiService/Movie.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DatabaseIntegrationBase.ApiService;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Gender { get; set; }
+    public bool IsActive { get; set; }
+}
